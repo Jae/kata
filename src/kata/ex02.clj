15843076aadfb83b78f59cd1fd16473effd82b2d
Jae <jlee@yetitrails.com>
2015-07-23 22:03:12 +0100

    karate chop

---
+++ src/kata/ex02.clj
@@ -0,0 +1,18 @@
+(ns ^{:doc "http://codekata.com/kata/kata02-karate-chop"}
+  kata.ex02)
+
+(defn chop
+  [needle haystack]
+  (loop [from 0 to (count haystack)]
+    (if-not (> (- to from) 1)
+      (if (= (get haystack from) needle) from -1)
+      (let [middle (int (/ (+ from to) 2))
+            at-middle (get haystack middle)]
+        (cond
+         (= at-middle needle) middle
+         (< at-middle needle) (recur (+ middle 1) to)
+         (> at-middle needle) (recur from middle))))))
+
+(comment
+  (int (/ (+ 0 (count [1 3])) 2))
+  (int (/ (+ 0 (count [1 3 5])) 2)))
